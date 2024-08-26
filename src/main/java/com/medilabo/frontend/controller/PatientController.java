fe776aa46dd453f1dad6e012f99d939f59a9e982
sully <delemos.sullivan@gmail.com>
2024-08-26 11:59:55 +0200

    Add logs

--- src/main/java/com/medilabo/frontend/controller/PatientController.java
+++ src/main/java/com/medilabo/frontend/controller/PatientController.java
@@ -1,30 +1,46 @@
 package com.medilabo.frontend.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@CrossOrigin(origins = "*")
+@RestController
 public class PatientController {
+
+    // Logger SLF4J
+    private static final Logger logger = LoggerFactory.getLogger(PatientController.class);
 
     @Value("${gateway.url}")
     private String gatewayUrl;
 
     private final RestTemplate restTemplate;
 
-    public PatientController() {
-        this.restTemplate = new RestTemplate();
+    public PatientController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/patients")
-    public String getPatients(Model model) {
-        List<Map<String, Object>> patients = restTemplate.getForObject(gatewayUrl + "/api/patients", List.class);
-        model.addAttribute("patients", patients);
-        return "patients";
+    public ResponseEntity<?> getPatients() {
+        logger.info("Request received to fetch patients data.");
+        logger.debug("Gateway URL: {}", gatewayUrl);
+
+        try {
+            logger.info("Sending request to: {}/api/patients", gatewayUrl);
+            List<Map<String, Object>> patients = restTemplate.getForObject(gatewayUrl + "/api/patients", List.class);
+            logger.info("Received response with patients data: {}", patients);
+            return ResponseEntity.ok(patients);
+        } catch (Exception e) {
+            logger.error("Error occurred while fetching patients data from the gateway", e);
+            return ResponseEntity.internalServerError().body("Error fetching patients data");
+        }
     }
 }
